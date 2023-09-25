2061a93775a2486b496dace7ffcd79d988447827
Taylor Riley <t_riley13@icloud.com>
2023-09-25 15:02:39 -0600

    working renderTopPicks

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-sources = ["http://localhost:3000/topRanked", "http://localhost:3000/dannyPicks", "http://localhost:3000/taylorPicks"]
+const sources = ["http://localhost:3000/topRanked", "http://localhost:3000/dannyPicks", "http://localhost:3000/taylorPicks"]
 function fetchContent(url) {
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        data.forEach(anime => {
+            renderTopPicks(anime)
+        })
+    })
 }
+fetchContent("http://localhost:3000/topRanked")
+// document.addEventListener("DOMContentLoaded", ()=>{
+//     sources.forEach((source) => fetchContent(source))
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    sources.forEach((source) => fetchContent(source))
-})
+// })
+
+const renderTopPicks = (anime)=>{
+    let img = document.createElement("img")
+    img.src = anime.image
+    // img.addEventListener("click", ()=>{
+
+    // })
+    document.querySelector("#top-picks").append(img)
+}
