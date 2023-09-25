062314d1ef6893d38bb3f3f1b7ee5b2d82388c36
Taylor Riley <t_riley13@icloud.com>
2023-09-25 15:42:41 -0600

    finished rendering picks

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
+const topRanked = "http://localhost:3000/topRanked"
+const dannyPicks = "http://localhost:3000/dannyPicks"
+const taylorPicks = "http://localhost:3000/taylorPicks"
 const sources = ["http://localhost:3000/topRanked", "http://localhost:3000/dannyPicks", "http://localhost:3000/taylorPicks"]
-function fetchContent(url) {
-    fetch(url)
+document.addEventListener("DOMContentLoaded", ()=>{
+    fetch(topRanked)
     .then(res => res.json())
     .then(data => {
         data.forEach(anime => {
             renderTopPicks(anime)
         })
     })
-}
-fetchContent("http://localhost:3000/topRanked")
-// document.addEventListener("DOMContentLoaded", ()=>{
-//     sources.forEach((source) => fetchContent(source))
-
-// })
+    fetch(taylorPicks)
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(anime => {
+            renderTaylorPicks(anime)
+        })
+    })
+    fetch(dannyPicks)
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(anime => {
+            renderDannyPicks(anime)
+        })
+    })
+})
 
 const renderTopPicks = (anime)=>{
     let img = document.createElement("img")
@@ -21,4 +33,21 @@ const renderTopPicks = (anime)=>{
 
     // })
     document.querySelector("#top-picks").append(img)
+}
+const renderTaylorPicks = (anime)=>{
+    let img = document.createElement("img")
+    img.src = anime.image
+    // img.addEventListener("click", ()=>{
+
+    // })
+    document.querySelector("#taylor-picks").append(img)
+}
+
+const renderDannyPicks = (anime)=>{
+    let img = document.createElement("img")
+    img.src = anime.image
+    // img.addEventListener("click", ()=>{
+
+    // })
+    document.querySelector("#danny-picks").append(img)
 }
